886744d69a4e9771f7ebebce47ab9a85d608d13c
Sergey Upirovets <sergey1b.u@gmail.com>
2018-03-04 13:22:50 +0300

    17/3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+        var i, l=str.length,  char,  last, stack=[];
+
+         for(i=0; i<l; i++){
+             char=str[i];
+             if(char=="{" || char=="(" || char =="["){
+                 stack.push(char);
+                 last=char;
+             }else if(char == '}' || char == ")" || char =="]"){
+                 if(last){
+                     if((char == '}' && last == "{") || (char == ')' && last == '(') || (char == ']' && last == '['))
+                       {
+                           stack.pop();
+                           last = stack.length > 0 ? stack[stack.length - 1] : undefined;
+                       }
+                 }else{
+                     return false;
+                 }
+             }
+         }
+         return stack.length==0;
+};
